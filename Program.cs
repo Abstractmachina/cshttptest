3b35efabc8dc01f51f6eaeb0aa3715eb60b4ca76
tc1421 <tc1421@ic.ac.uk>
2023-06-11 23:59:38 +0900

    text to image POST request working

--- Program.cs
+++ Program.cs
@@ -3,82 +3,96 @@ using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Linq;
+using System.Net.Http.Headers;
 
 using (HttpClient client = new())
 {
-    DotNetEnv.Env.Load();
-    string apiKey = Environment.GetEnvironmentVariable("API_KEY");
+    try{
+        DotNetEnv.Env.Load();
+        string apiKey = Environment.GetEnvironmentVariable("API_KEY");
 
 
-    var newPrompt = new TextToImagePrompt(apiKey);
+        var newPrompt = new TextToImagePrompt(apiKey);
 
-    var settings = new JsonSerializerSettings{
-        Converters = { new T2IJsonConverter() }
-     };
+        var settings = new JsonSerializerSettings{
+            Converters = { new T2IJsonConverter() }
+        };
 
-    StringContent body = new(JsonConvert.SerializeObject(newPrompt, settings));
+        StringContent body = new(JsonConvert.SerializeObject(newPrompt, settings));
+        body.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        // StringContent body = new(JsonConvert.SerializeObject(newPrompt));
 
-    // StringContent body = new(JsonConvert.SerializeObject(newPrompt));
+        Console.WriteLine(await body.ReadAsStringAsync());
 
-    Console.WriteLine(await body.ReadAsStringAsync());
-    return;
-
-    HttpResponseMessage response = await client.GetAsync("https://official-joke-api.appspot.com/random_ten");
-
-    if (response.IsSuccessStatusCode)
-    {
-        string content = await response.Content.ReadAsStringAsync();
-
-        List<Joke>? jokes = JsonConvert.DeserializeObject<List<Joke>>(content);
-
-        if (jokes == null)
-            return;
-
-        foreach (Joke joke in jokes)
+        HttpResponseMessage response = await client.PostAsync("https://stablediffusionapi.com/api/v3/text2img", body);
+        if (!response.IsSuccessStatusCode)
         {
-            Console.WriteLine(joke.Setup);
-            Console.WriteLine(joke.Punchline);
-            Console.WriteLine();
+            var content = await response.Content.ReadAsStringAsync();
+            throw new Exception(content);
         }
+
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
+    } 
+    catch (Exception err) {
+        Console.WriteLine(err.ToString());
     }
 }
 
-public class Joke
-{
-    public string Setup { get; set; }
-    public string Punchline { get; set; }
-}
 
 public class TextToImagePrompt
 {
     public string Key { get; set; }
     public string Prompt { get; set; }
-    public string Negative_prompt { get; set; }
-    public string Width { get; set; }
-    public string Height { get; set; }
-    public string Samples { get; set; }
-    public string Num_inference_steps { get; set; }
-    public string Safety_checker { get; set; }
-    public string Enhance_prompt { get; set; }
-    public string Seed { get; set; }
-    public string Guidance_scale { get; set; }
-    public string Multi_lingual { get; set; }
-    public string Panorama { get; set; }
-    public string Self_attention { get; set; }
-    public string Upscale { get; set; }
+    public string? Negative_prompt { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public int Samples { get; set; }
+    public int Num_inference_steps { get; set; }
+    public bool Safety_checker { get; set; }
+    public bool Enhance_prompt { get; set; }
+    public int? Seed { get; set; }
+    public float Guidance_scale { get; set; }
+    public bool Multi_lingual { get; set; }
+    public bool Panorama { get; set; }
+    public bool Self_attention { get; set; }
+    public bool Upscale { get; set; }
     public string Embedding_model { get; set; }
-    public string Webhook { get; set; }
-    public string Track_id { get; set; }
+    public string? Webhook { get; set; }
+    public string? Track_id { get; set; }
 
-    public TextToImagePrompt(string key) {
-        Key = key;
-        Prompt = "a round, red apple";
-    }
+    public TextToImagePrompt(string key) : this(key, "a round, red apple") {}
+
+    public TextToImagePrompt(string key, string prompt) : this(key, prompt, null, 512, 512) {}
+
+    public TextToImagePrompt(string key, string prompt, string? negative_prompt, int width, int height) : this(key, prompt, negative_prompt, width, height, 1, 20) {}
     
-    public TextToImagePrompt(string key, string prompt, string negative_prompt,
-    int width, int height, int samples, int num_inference_steps, bool safety_checker, bool enhance_prompt, int seed, float guidance_scale, bool multi_lingual, bool panorama, bool self_attention, bool upscale, string embeddings_model, string webhook, string track_id)
-    {
+    public TextToImagePrompt(
+        string key, string prompt, string negative_prompt, int width, int height, int samples, int num_inference_steps
+    ) : this(
+        key, prompt, negative_prompt, width, height, samples, num_inference_steps, false, true, null, 7.5f, false, false, false, false, "embeddings_model_id", null, null
+    ){
+    }
 
+    public TextToImagePrompt(string key, string prompt, string negative_prompt, int width, int height, int samples, int num_inference_steps, bool safety_checker, bool enhance_prompt, int? seed, float guidance_scale, bool multi_lingual, bool panorama, bool self_attention, bool upscale, string embeddings_model, string? webhook, string? track_id)
+    {
+        Key = key;
+        Prompt = prompt;
+        Negative_prompt = negative_prompt;
+        Width = width;
+        Height = height;
+        Samples = samples;
+        Num_inference_steps = num_inference_steps;
+        Safety_checker = safety_checker;
+        Enhance_prompt = enhance_prompt;
+        Seed = seed;
+        Guidance_scale = guidance_scale;
+        Multi_lingual = multi_lingual;
+        Panorama = panorama;
+        Self_attention = self_attention;
+        Upscale = upscale;
+        Embedding_model = embeddings_model;
+        Webhook = webhook;
+        Track_id = track_id;
     }
 }
 
@@ -105,19 +119,17 @@ class T2IJsonConverter : JsonConverter
         {
             var propertyName = contractResolver != null ? contractResolver.GetResolvedPropertyName(property.Name) : property.Name;
             var propertyValue = property.GetValue(value);
-            if (propertyValue == null) continue; 
-            jsonObject.Add(propertyName.ToLower(), JToken.FromObject(propertyValue));
-        }
+            if (propertyValue == null) {
+                jsonObject.Add(propertyName.ToLower(), JValue.CreateNull());
+            }
+            else if (propertyValue is bool ) {
+                jsonObject.Add(propertyName.ToLower(), 
+                ((bool)propertyValue == true) ? "yes" : "no");
 
-        // // Modify the "LastName" value
-        // var lastNameProperty = jsonObject.Property("lastname");
-        // if (lastNameProperty != null)
-        // {
-        //     lastNameProperty.Value = lastNameProperty.Value.ToString().ToUpper();
-        // }
-        Console.WriteLine(jsonObject.HasValues);
-        foreach (var v in jsonObject) {
-            Console.WriteLine(v);
+            }
+            else {
+                jsonObject.Add(propertyName.ToLower(), JToken.FromObject(propertyValue));
+            }
         }
         jsonObject.WriteTo(writer);
     }
